01a8b1def99e8beef23db44cb8f1a3f1ac790967
Stephanie Thomas <stephie.thomas@gmail.com>
2022-02-09 23:00:12 -0600

    post

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const postRoute = require('./post-routes');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+
 
 module.exports = router;
+
